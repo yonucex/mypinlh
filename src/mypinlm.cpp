6125183cd2d7018bf01b88fd1bced8adb4b8b73c
yonucex <yonucex@gmail.com>
2023-12-31 09:43:58 +0700

    test1

--- src/mypinlm.cpp
+++ src/mypinlm.cpp
@@ -1,45 +1,15 @@
-#include "mypinlm.h"
+#include "MyPinLM.h"
 
-// คอนสตรักเตอร์ รับ ID ของอุปกรณ์และกำหนดให้กับตัวแปร deviceID
-MYPINLM::MYPINLM(uint8_t id) : deviceID(id) {}
-
-// เริ่มต้นการสื่อสาร Modbus โดยกำหนด baud rate และ config สำหรับ Serial
-void MYPINLM::begin(long baudRate, uint8_t config) {
-    Serial2.begin(baudRate, config); // เริ่มต้นการสื่อสารโดยใช้ Serial2
-    node.begin(deviceID, Serial2);   // กำหนด ID ของอุปกรณ์และ Serial port
+MyPinLM::MyPinLM(uint8_t deviceID, Stream &port) : _deviceID(deviceID), node() {
+  node.begin(deviceID, port);
 }
 
-// อ่านค่าน้ำหนักจากอุปกรณ์
-float MYPINLM::readWeight() {
-    uint16_t weight;
-    // ส่งคำสั่งอ่านค่าน้ำหนักจาก register พิเศษ
-    node.readHoldingRegisters(0x01, 1);
-    // ตรวจสอบว่าการอ่านข้อมูลสำเร็จหรือไม่
-    if (node.getResponseBuffer(0) != 0xFFFF) {
-        weight = node.getResponseBuffer(0);
-    } else {
-        weight = 0; // หรือจัดการข้อผิดพลาดตามที่คุณต้องการ
-    }
-    return weight;
+void MyPinLM::begin(long baudRate) {
+  // Set up the baud rate for Serial communication
 }
 
-// อ่านพารามิเตอร์จากอุปกรณ์จาก address ที่กำหนด
-uint16_t MYPINLM::readParameter(uint16_t address) {
-    uint16_t value;
-    // ส่งคำสั่งอ่านพารามิเตอร์
-    node.readHoldingRegisters(address, 1);
-    // ตรวจสอบว่าการอ่านข้อมูลสำเร็จหรือไม่
-    if (node.getResponseBuffer(0) != 0xFFFF) {
-        value = node.getResponseBuffer(0);
-    } else {
-        value = 0; // หรือจัดการข้อผิดพลาดตามที่คุณต้องการ
-    }
-    return value;
-}
-
-// เขียนพารามิเตอร์ไปยัง address ที่กำหนดบนอุปกรณ์
-uint16_t MYPINLM::writeParameter(uint16_t address, uint16_t value) {
-    uint8_t result = node.writeSingleRegister(address, value);
-    // ที่นี่คุณสามารถเพิ่มการตรวจสอบความสำเร็จของการเขียนข้อมูล
-    return result; // ส่งค่ากลับเพื่อยืนยันการเขียนข้อมูล
+float MyPinLM::readWeight() {
+  // Implement the function to read weight from the Modbus device
+  // This is a placeholder and should be filled with actual implementation
+  return 0.0;
 }
--- src/mypinlm.h
+++ src/mypinlm.h
@@ -1,21 +1,18 @@
-#ifndef MYPINLM_H
-#define MYPINLM_H
+#ifndef MyPinLM_h
+#define MyPinLM_h
 
+#include <Arduino.h>
 #include <ModbusMaster.h>
 
-// คลาสสำหรับการจัดการการสื่อสาร Modbus กับ MYPIN LM
-class MYPINLM {
-private:
-    ModbusMaster node; // ตัวแปรสำหรับการจัดการ Modbus
-    uint8_t deviceID;  // ID ของอุปกรณ์บนเครือข่าย Modbus
+class MyPinLM {
+  public:
+    MyPinLM(uint8_t deviceID, Stream &port); // Constructor
+    void begin(long baudRate);
+    float readWeight(); // Placeholder for function to read weight
 
-public:
-    MYPINLM(uint8_t id); // คอนสตรักเตอร์ที่รับ ID ของอุปกรณ์
-
-    void begin(long baudRate, uint8_t config); // เริ่มการสื่อสาร
-    float readWeight(); // อ่านค่าน้ำหนัก
-    uint16_t readParameter(uint16_t address); // อ่านพารามิเตอร์จากอุปกรณ์
-    uint16_t writeParameter(uint16_t address, uint16_t value); // เขียนพารามิเตอร์ไปยังอุปกรณ์
+  private:
+    ModbusMaster node;
+    uint8_t _deviceID;
 };
 
-#endif // MYPINLM_H
+#endif // MyPinLM_h
